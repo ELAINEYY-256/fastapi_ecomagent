0a8d79d778bf85b7d8e72e4a43d627bd08da3048
rkimeraa94 <rkimera2013@gmail.com>
2025-12-04 17:05:03 +0300

    fix:add user to follow dry principles

--- api_project/app/controllers/user_controller.py
+++ api_project/app/controllers/user_controller.py
@@ -1,10 +1,85 @@
 from app.services.user_service import UserService
+from fastapi import HTTPException, status
+from app.models.user import User
+from app.models.branch import Branch
+from app.models.company import Company
+from app.utils.security import create_access_token
+
 
 class UserController:
 
     @staticmethod
-    def register_user(name: str, email: str, password: str, role: str):
-        return UserService.register_user(name, email, password, role)
+    def signup_company(data):
+        # check if company exists
+        company = UserService.get_company_by_name(data.company_name)
+        if company:
+            raise HTTPException(
+                status_code=400, detail="Company with this name already exists"
+            )
+
+        # create company
+        company = Company(name=data.company_name)
+        newCompany = UserService.create_company(company)
+
+        if newCompany:
+            # create head branch
+            headBranch = Branch(
+                name=f"{newCompany.name} Head Office", company_id=newCompany.id
+            )
+            newHeadBranch = UserService.create_branch(headBranch)
+            # create super user
+            if newHeadBranch:
+                # create user
+                user = User(
+                    is_superuser=True,
+                    first_name=data.first_name,
+                    email=data.email,
+                    password_hash=data.password,
+                    role="superuser",
+                    company_id=newCompany.id,
+                    branch_id=newHeadBranch.id,
+                )
+                newUser = UserService.create_user(user)
+                if newUser:
+                    # create token
+                    token = create_access_token(
+                        {
+                            "sub": newUser.email,
+                            "user_id": newUser.id,
+                            "company_id": newCompany.id,
+                            "role": newUser.role,
+                        }
+                    )
+                    return {
+                        "company_id": newCompany.id,
+                        "user_id": newUser.id,
+                        "email": newUser.email,
+                        "role": newUser.role,
+                        "token": token,
+                    }
+        return None
+
+    @staticmethod
+    def register_user(
+        name: str, email: str, password: str, role: str, is_superuser: bool = False
+    ):
+
+        # check if user exists
+        user = UserService.get_user_by_email(email)
+        if user:
+            raise HTTPException(
+                status_code=400, detail="User with this email already exists"
+            )
+
+        # create user
+        user = User(
+            is_superuser=is_superuser,
+            first_name=name,
+            email=email,
+            password_hash=password,
+            role=role,
+        )
+        return UserService.create_user(user)
 
     @staticmethod
     def login_user(email: str, password: str):
--- api_project/app/services/user_service.py
+++ api_project/app/services/user_service.py
@@ -2,42 +2,69 @@ from sqlalchemy.orm import Session
 from fastapi import HTTPException, status
 from app.database import SessionLocal
 from app.models.user import User
+from app.models.company import Company
 from app.utils.security import hash_password, verify_password, create_access_token
 
 
 class UserService:
-
+    
     @staticmethod
-    def register_user(name: str, email: str, password: str, role: str):
+    def get_company_by_name (name: str):
         db: Session = SessionLocal()
-
+        return db.query(Company).filter(Company.name == name).first()
+    
+    @staticmethod
+    def create_company(company: Company):
         try:
-            # Check if user exists
-            if db.query(User).filter(User.email == email).first():
-                raise HTTPException(
-                    status_code=status.HTTP_400_BAD_REQUEST,
-                    detail="User with this email already exists."
-                )
-
-            user = User(
-                name=name,
-                email=email,
-                password_hash=hash_password(password),
-                role=role
-            )
-
-            db.add(user)
+            db: Session = SessionLocal()
+            db.add(company)
             db.commit()
-            db.refresh(user)
-            return user
-
+            db.refresh(company)
+            return company
         except Exception as e:
             db.rollback()
             raise HTTPException(
                 status_code=500,
                 detail=str(e)
             )
-
+        finally:
+            db.close()
+    
+    @staticmethod
+    def get_user_by_email(email: str):
+        db: Session = SessionLocal()
+        return db.query(User).filter(User.email == email).first()
+    
+    @staticmethod
+    def get_user_by_id(user_id: int):
+        db: Session = SessionLocal()
+        return db.query(User).filter(User.id == user_id).first()
+    
+    @staticmethod
+    def get_users_by_company_id(company_id: int):
+        db: Session = SessionLocal()
+        return db.query(User).filter(User.company_id == company_id).all()
+    
+    @staticmethod
+    def get_users_by_branch_id(branch_id: int):
+        db: Session = SessionLocal()
+        return db.query(User).filter(User.branch_id == branch_id).all()
+    
+    
+    @staticmethod
+    def create_user(user: User):
+        db: Session = SessionLocal()
+        try:
+            db.add(user)
+            db.commit()
+            db.refresh(user)
+            return user
+        except Exception as e:
+            db.rollback()
+            raise HTTPException(
+                status_code=500,
+                detail=str(e)
+            )
         finally:
             db.close()
 
